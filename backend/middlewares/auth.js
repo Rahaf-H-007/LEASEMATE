6f7bc19fde18f8eaab5a3f2b8906949c16a462bd
Rahaf Hazem <rahafhazem2002@gmail.com>
2025-07-13 17:31:10 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -34,4 +34,12 @@ const protect = async (req, res, next) => {
     }
 };
 
-module.exports = { protect };
+const admin = async (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Not authorized as admin' });
+    }
+};
+
+module.exports = { protect, admin };
