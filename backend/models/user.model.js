96439735394bf3fd6d1a2abd57d5c97f9fc55d85
NadeenElGendi <nadineamrelgendi@gmail.com>
2025-07-18 11:47:02 +0300

    update user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    email: { type: String, unique: true, sparse: true, lowercase: true, trim: true },
+    username: { type: String, unique: true, sparse: true, lowercase: true, trim: true },
     phone: { type: String, unique: true, sparse: true, trim: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['landlord', 'tenant', 'admin'], default: 'tenant' },
