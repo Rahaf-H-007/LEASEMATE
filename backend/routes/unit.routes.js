42f722095e60503b61aebea7e53652f11a3882f9
NadeenElGendi <nadineamrelgendi@gmail.com>
2025-07-19 22:34:40 +0300

    unit routes

---
+++ backend/routes/unit.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUnits,
+  getUnit,
+  addUnit,
+  updateUnit,
+  deleteUnit,
+  deleteUnitImage,
+  testDatabase,
+  getMyUnits,
+} = require("../controllers/unit.controller");
+
+const upload = require("../middlewares/upload.middleware");
+const { protect } = require("../middlewares/auth.middleware");
+const { checkRole } = require("../middlewares/role.middleware");
+
+router
+  .route("/")
+  .get(getAllUnits) // Public access for viewing units
+  .post(protect, checkRole("landlord"), upload.array("images", 5), addUnit); // Auth required for creating
+
+router.get("/my-units", protect, checkRole("landlord"), getMyUnits);
+
+router
+  .route("/:id")
+  .get(getUnit) // Public access for viewing individual units
+  .patch(protect, checkRole("landlord"), upload.array("images", 5), updateUnit) // Auth required for updating
+  .delete(protect, checkRole("landlord"), deleteUnit); // Auth required for deleting
+
+router.delete("/:id/image", protect, checkRole("landlord"), deleteUnitImage);
+
+module.exports = router;
